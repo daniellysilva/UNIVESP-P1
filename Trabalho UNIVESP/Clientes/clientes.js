84a7ef95a1f951ff1d2673aeec19ea0690dc5081
Danielly Alves Silva <101145664+daniellysilva@users.noreply.github.com>
2024-05-07 17:06:51 -0300

    1

--- Trabalho UNIVESP/Clientes/clientes.js
+++ Trabalho UNIVESP/Clientes/clientes.js
@@ -1,15 +1,46 @@
-class Cliente{
+class Cliente {
 
-    constructor(){
-        this.id = 0;
-        this.nomecompleto = '';
-        this.tipodocliente = '';
-        this.datainicio = '';
-        this.andamento = '';
+    constructor() {
+        this.id = 1;
+        this.arrayClientes = [];
     }
 
-    adicionar(){
-        alert('Danielly');
+    adicionar() {
+      let cliente =  this.lerDados();
+
+     if (this.validaCampos(cliente)){
+        alert('salvar')
+     }
+        console.log(cliente);
+    }
+
+    lerDados() {
+        let cliente = {}
+
+        cliente.id = this.id;
+        cliente.nomeCompleto = document.getElementById('nomecompleto').value;
+        cliente.andamento = document.getElementById('andamento').value;
+
+        return
+    }
+
+    validaCampos(cliente){
+        let  msg = '';
+
+        if (cliente.nomeCompleto == ''){
+            msg +- '- Informe o nome completo \n';
+        }
+
+        if (cliente.andamento == ''){
+            msg +- '- Informe o andamento \n';
+        }
+
+        if (msg != ''){
+           alert(msg);
+           return false
+        }
+
+        return true;
     }
 }
 
