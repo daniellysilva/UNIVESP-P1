b06e2d7326932e66a9ad1b19042ccba51bf6086c
Danielly Alves Silva <101145664+daniellysilva@users.noreply.github.com>
2024-05-07 15:33:34 -0300

    colocando js

---
+++ Trabalho UNIVESP/Clientes/clientes.js
@@ -0,0 +1,16 @@
+class Cliente{
+
+    constructor(){
+        this.id = 0;
+        this.nomecompleto = '';
+        this.tipodocliente = '';
+        this.datainicio = '';
+        this.andamento = '';
+    }
+
+    adicionar(){
+        alert('Danielly');
+    }
+}
+
+var cliente = new Cliente();
